8266d738ce4a3e4a1fd5d0c251c350b5892f41cc
TymonScibiorski <tym9128@gmail.com>
2025-03-10 15:53:44 +0100

    Wrote one of the validity checks for isPlaceValid - canRotateSomething

--- src/Board.java
+++ src/Board.java
@@ -27,26 +27,25 @@ public class Board {
 
     private boolean isPlaceValid(int x, int y, int colour) {
         //podejscie nr 1
-        //bool is empty
-        //bool czy przylega do chociaż jednego nieswojego (xy swój i xy tego nieswojego) -> <-
-        //bool czy możesz go obrócić
-        //if ze wszystkich
-
-        boolean isOnEmptySpace = board[y][x] != 0;
-        boolean isNextToOtherColor = touchesOtherColor(x, y, colour);
-        boolean isNextToOtherColor2;
-        boolean isNextToOtherColor3;
-        boolean isNextToOtherColor4;
+        //Pod koniec if ze wszystkich
 
         //podejscie nr2
         //ciag ifów które sprwdzają warunke, jeśli nie valid to return false
         //na końcu funkcji return true
-        if (isOnEmptySpace) {
+
+        boolean isSpaceEmpty = board[y][x] == 0;
+        boolean isNextToOtherColor = touchesOtherColor(x, y, colour);
+        boolean canRotateSomething = canRotateSomething(x, y, colour);
+
+
+        if (!isSpaceEmpty) {
             return false;
         }
-        //is place empty
         return true;
         //todo: add additional validity checks
+        //bool is empty DONE
+        //bool czy przylega do chociaż jednego nieswojego (xy swój i xy tego nieswojego) -> <- DONE
+        //bool czy może coś obrócić
     }
 
     private boolean touchesOtherColor(int x, int y, int colour) {
@@ -94,6 +93,34 @@ public class Board {
             }
             if (x != 8) {
                 if (board[y+1][x+1] == otherColour) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean canRotateSomething(int x, int y, int colour) {
+        //vertical, horizontal and diagonal
+        //searching for the same colour
+        //not touching xy
+
+        for (int i = 0; i < size; i++) {
+            if (i != x-1 && i != x+1 && board[y][i] == colour){
+                return true;
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (i != y-1 && i != y+1 && board[i][x] == colour){
+                return true;
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if(i != y-1 && i != y+1 && j != x-1 && j != x+1 && board[i][j] == colour){
                     return true;
                 }
             }
